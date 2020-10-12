83957582d2133b82fb47e8738caf0aac106ebc74
Justin Turner <48540310+ProjectGrantwood@users.noreply.github.com>
2020-10-12 10:59:56 -0400

    Implemented grid / subgrid layouts

--- rpg.js
+++ rpg.js
@@ -1,25 +1,36 @@
 const Player = {
-    name: "Bobwald Smithwurst",
-    statStrings: statStrings = ["Knowledge", "Reasoning", "Strength", "Agility", "Awareness"],
+    name: 'Bobwald Smithwurst',
+    experience: 0,
+    level: 1,
+    stats: {
+        Knowledge: 0,
+        Reasoning: 0,
+        Strength: 0,
+        Agility: 0,
+        Awareness: 0
+    }
 }
 
+const playerStatStrings = ['Knowledge', 'Reasoning', 'Strength', 'Agility', 'Awareness'];
+
 Player.initStats = function () {
-    let stats = partition(25, 5);
-    for (let i = 0; i < stats.length; i++) {
-        Player[Player.statStrings[i]] = stats[i];
+    let stats = partition(20, 5, 6);
+    for (let i = 0; i < 5; i++) {
+        Player[playerStatStrings[i]] = stats[i];
     }
 }
 
 Player.showName = function () {
-    document.getElementById("playername").innerHTML = `${Player.name}`;
+    document.getElementById('playername').innerHTML = `<header>${Player.name}</header>`;
+    document.getElementById('playerinfo').innerHTML = `Level: ${Player.level}<br>Experience: ${Player.experience}`
 }
 
 Player.showStats = function () {
     let showStats = ``;
     for (let i = 0; i < 5; i++) {
-        showStats += `<br>${statStrings[i]}: ${Player[Player.statStrings[i]]}`;
+        showStats += `${playerStatStrings[i]}: ${Player[playerStatStrings[i]]}<br>`;
     }
-    document.getElementById("stats").innerHTML = showStats;
+    document.getElementById('stats').innerHTML = `<header>${Player.name}'s Stats:</header><br>${showStats}`;
 }
 
 Player.initStats();
--- utility.js
+++ utility.js
@@ -1,8 +1,10 @@
-function partition(val, amt) {
+function partition(val, amt, max) {
     let valCopy = val;
     let ratio = valCopy / (amt - 1)
     if (ratio < 1) {
-        console.log('function partition() failed: argument "val" must be greater than or equal to argument "amt"')
+        console.log('function partition() failed: argument "val" must be greater than or equal to argument "amt"');
+    } else if (max < ratio) {
+        console.log('function partition() failed: argument "max" must be greater than or equal to argument "val" divided by one less than argument "amt"')
     } else {
         let arrayOfVals = [];
         while (arrayOfVals.length < amt - 1) {
@@ -17,6 +19,14 @@ function partition(val, amt) {
                     if (i !== j && arrayOfVals[j] > arrayOfVals[i] + 1) {
                         arrayOfVals[j] -= 1;
                         arrayOfVals[i] += 1;
+                    }
+                }
+            }
+            while (arrayOfVals[i] >= max) {
+                for (let j = arrayOfVals.length - 1; j >= 0; j--) {
+                    if (i !== j && arrayOfVals[j] < arrayOfVals[i] - 1) {
+                        arrayOfVals[j] += 1;
+                        arrayOfVals[i] -= 1;
                     }
                 }
             }
